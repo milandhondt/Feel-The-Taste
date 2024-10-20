d47ed3b57152733df55e6f69acd976e488e03f1b
Milan Dhondt <milan.dhondt@student.hogent.be>
2024-10-20 20:47:43 +0200

    facebook link toevoegen

--- Feel-The-Taste/src/components/Footer.jsx
+++ Feel-The-Taste/src/components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
               Contact
             </Text>
             <Text fontSize={'lg'} color="#FFFDE7">
-              Email: <Link href="mailto:info@feelthetaste.be"  color="#FFFDE7" textDecoration="underline">
+              Email: <Link href="mailto:info@feelthetaste.be" color="#FFFDE7" textDecoration="underline">
                 info@feelthetaste.be</Link>
             </Text>
             <Text fontSize={'lg'} color="#FFFDE7">Telefoon: +32 470 42 55 86</Text>
@@ -35,7 +35,8 @@ const Footer = () => {
           <Text fontSize="2xl" fontWeight="bold" mb={2} color="#FFFDE7">
             Volg ons!
           </Text>
-          <Link href="https://facebook.com" isExternal color="#FFFDE7" mr={4} display="flex" alignItems="center">
+          <Link href="https://www.facebook.com/profile.php?id=61567379101819" isExternal color="#FFFDE7"
+            mr={4} display="flex" alignItems="center">
             <FaFacebook style={{ marginRight: '4px' }} />
             <Text fontSize={'lg'}>Facebook</Text>
           </Link>
